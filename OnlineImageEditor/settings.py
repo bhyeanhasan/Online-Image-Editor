55531df75e5bdb5a06919719d960d34e8bc99f60
Md. Babul Hasan (NoYoN) <bhyean16@cse.pstu.ac.bd>
2023-06-02 02:24:54 +0600

    last

--- OnlineImageEditor/settings.py
+++ OnlineImageEditor/settings.py
@@ -82,7 +82,8 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_files'), ]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'static_files')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
